50d2aa8ffdb5f6e3a06e1273c146dffebb54de77
Mike Gedelman <mgedelman@aledade.com>
2021-12-04 10:35:40 -0700

    part3 finished

--- 3.cpp
+++ 3.cpp
@@ -4,19 +4,10 @@
 
 struct DiagnosticInfo {
     std::vector<std::string> lines;
-    std::vector<int> count_of_1s;
-    int num_lines;
-};
-
-struct PowerDiag {
-    std::string gamma;
-    std::string epsilon;
+    int num_lines = 0;
 };
 
 DiagnosticInfo process_input() {
-    bool initialized = false;
-    // int num_lines = 0;
-    // std::vector<int> count_of_1s;
     DiagnosticInfo result;
 
     for(std::string line; std::getline(std::cin, line);) {
@@ -24,32 +15,31 @@ DiagnosticInfo process_input() {
         result.num_lines += 1;
     }
 
-    for(auto &line: result.lines) {  
-        // this is ugly but whatever - don't see an easy way to peek at
-        // stdin input
-        if (!initialized) {
-            result.count_of_1s.resize(line.size(), 0);
-        }
+    return result;
+}
 
+std::vector<int> get_count_of_1s(std::vector<std::string> lines) {
+    std::vector<int> count_of_1s(lines[0].size());
+    for(auto &line: lines) {  
         int pos = 0;
         for (auto &bit: line) {
             if (bit == '1') {
-                // std::cout << "here " << pos << std::endl;
-                result.count_of_1s[pos] += 1;
+                count_of_1s[pos] += 1;
             }
             pos += 1;
         }
     }
 
-    return result;
+    return count_of_1s;
 }
 
-PowerDiag part1(std::vector<int> count_of_1s, int num_lines) {
+void part1(DiagnosticInfo &diag) {
+    std::vector<int> count_of_1s = get_count_of_1s(diag.lines);
+
     std::string gamma = "";
     std::string epsilon = "";
     for (int i = 0; i < count_of_1s.size(); i++) {
-        std::cout << count_of_1s[i] << " ";
-        if (count_of_1s[i] > num_lines / 2) {
+        if (count_of_1s[i] > (diag.num_lines - count_of_1s[i])) {
             gamma += "1";
             epsilon += "0";
         } else {
@@ -57,40 +47,49 @@ PowerDiag part1(std::vector<int> count_of_1s, int num_lines) {
             epsilon += "1";
         }
     }
-    std::cout << std::endl;
 
-    unsigned long long gamma_decimal = std::strtoull(gamma.c_str(), 0, 2);
-    unsigned long long epsilon_decimal = std::strtoull(epsilon.c_str(), 0, 2);
-    std::cout << "Gamma: " << gamma_decimal << " " << gamma << std::endl;
-    std::cout << "Epsilon: " << epsilon_decimal << " " << epsilon << std::endl;
-
-    PowerDiag diag;
-    diag.gamma = gamma;
-    diag.epsilon = epsilon;
-
-    return diag;
+    std::cout << "Gamma: " << std::strtoull(gamma.c_str(), 0, 2) << " " << gamma << std::endl;
+    std::cout << "Epsilon: " << std::strtoull(epsilon.c_str(), 0, 2) << " " << epsilon << std::endl;
 }
 
-void part2(std::vector<std::string> &lines, std::string gamma, std::string epsilon) {
-    auto oxygen_candidates = std::vector(lines);
-    auto co2_candidates = std::vector(lines);
-    for (int i = 0; i < gamma.size(); i++) {
-        oxygen_candidates.erase(std::remove_if(
-            oxygen_candidates.begin(),
-            oxygen_candidates.end(), 
-            [i, gamma](std::string &line) { return line[i] != gamma[i]; }
-        ));
+std::string life_support_rating(DiagnosticInfo &diag, char (*determine_most_common_bit)(int, int)) {
+    std::vector<std::string> candidates(diag.lines);
+    int bit_position = 0;
+    while (candidates.size() > 1) {
+        std::vector<int> count_of_1s = get_count_of_1s(candidates);
+        char most_common_bit = determine_most_common_bit(count_of_1s[bit_position], candidates.size());
+
+        std::vector<std::string> new_candidates;
+        for (auto &candidate: candidates) {
+            if (candidate[bit_position] == most_common_bit) {
+                new_candidates.push_back(candidate);
+            }
+        }
+
+        candidates = new_candidates;
+        bit_position += 1;
     }
 
-    for (auto &line: oxygen_candidates) {
-        if (line.size() == 0) continue;
-        std::cout << line << std::endl;
-    }
+    return candidates[0];
+}
+
+char most_common_bit(int num_1s, int list_size) {
+    return num_1s >= (list_size - num_1s) ? '1' : '0';
+}
+
+char least_common_bit(int num_1s, int list_size) {
+    return num_1s < (list_size - num_1s) ? '1' : '0';
+}
+
+void part2(DiagnosticInfo &diag) {
+    std::string oxygen_rating = life_support_rating(diag, most_common_bit);
+    std::string co2_rating = life_support_rating(diag, least_common_bit);
+    std::cout << "Oxygen rating: " << std::strtoull(oxygen_rating.c_str(), 0, 2) << " " << oxygen_rating << std::endl;
+    std::cout << "CO2 rating: " << std::strtoull(co2_rating.c_str(), 0, 2) << " " << co2_rating << std::endl;
 }
 
 int main() {
-    // InputCounts counts = process_input()
     DiagnosticInfo diag = process_input();
-    PowerDiag power_diag = part1(diag.count_of_1s, diag.num_lines);
-    part2(diag.lines, power_diag.gamma, power_diag.epsilon);
+    part1(diag);
+    part2(diag);
 }
